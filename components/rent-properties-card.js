a6229c17e6e6a198127df0a2a41ec791eefff4cf
Muhammad Usman <usman.latif762@gmail.com>
2023-08-02 13:53:09 +0500

    updated

--- components/rent-properties-card.js
+++ components/rent-properties-card.js
@@ -6,10 +6,10 @@ const RentPropertiesCard = memo(() => {
     <div className="self-stretch flex flex-col py-[86px] px-0 items-center justify-start gap-[39px] text-center text-21xl text-primary-800 font-body-large-400">
       <div className="self-stretch flex flex-col items-center justify-start gap-[40px] max-w-[95%px]">
         <div className="w-[688px] flex flex-col items-center justify-start gap-[24px] max-w-[95%px] lg:max-w-[95%] md:self-stretch md:w-auto">
-          <div className="self-stretch relative leading-[48px] font-semibold">
+          <div className="self-stretch  leading-[48px] font-semibold">
             Latest Properties of Rent
           </div>
-          <div className="self-stretch relative text-xl leading-[28px] text-gray-400">
+          <div className="self-stretch  text-xl leading-[28px] text-gray-400">
             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
             posuere cubilia curae; Proin sodales ultrices nulla blandit
             volutpat.
@@ -23,7 +23,7 @@ const RentPropertiesCard = memo(() => {
         </div>
       </div>
       <button className="cursor-pointer [border:none] py-3 px-6 bg-primary-500 rounded flex flex-row items-start justify-start">
-        <div className="relative text-base leading-[24px] font-medium font-body-large-400 text-gray-white text-center">
+        <div className=" text-base leading-[24px] font-medium font-body-large-400 text-gray-white text-center">
           Load more listing
         </div>
       </button>
