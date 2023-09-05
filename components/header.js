f93305e404a58a723c3d0aed8d7cb06d7f5a9072
Muhammad Usman <usman.latif762@gmail.com>
2023-09-05 09:04:59 +0500

    updated sidebar animation

--- components/header.js
+++ components/header.js
@@ -83,7 +83,7 @@ const Header = memo(({ hamburger }) => {
       <div
         className={
           sidebaropen
-            ? `fixed top-0 left-0 bottom-0  duration-1000
+            ? `fixed top-0 left-0 bottom-0  duration-700
     p-2 w-[300px] overflow-y-auto text-center bg-gray-900 shadow sm:hidden h-screen`
             : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
         }
