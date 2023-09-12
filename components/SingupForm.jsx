bf69c62933da7ec3c35199a76ad51482f22ed7a5
Muhammad Usman <usman.latif762@gmail.com>
2023-09-12 22:02:56 +0500

    updated

--- components/SingupForm.jsx
+++ components/SingupForm.jsx
@@ -111,7 +111,7 @@ const SingupForm = () => {
                 </div>
             </div>
 
-            <section className="bg-gray-50 dark:bg-gray-900">
+            {/* <section className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8  md:h-screen lg:py-0">
 
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -148,7 +148,7 @@ const SingupForm = () => {
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> */}
         </>
     )
 }
