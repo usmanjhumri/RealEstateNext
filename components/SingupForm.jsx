31eb28c940b9e60d1bc00b06f86dec95521c0450
Muhammad Usman <usman.latif762@gmail.com>
2023-09-13 11:52:20 +0500

    updated

--- components/SingupForm.jsx
+++ components/SingupForm.jsx
@@ -1,35 +1,37 @@
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { ThreeDots } from 'react-loader-spinner'
-import { ToastContainer, toast } from 'react-toastify'
-import { useRouter } from 'next/navigation'
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
+import { ToastContainer, toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 const SingupForm = () => {
-
-    const Router = useRouter()
-    const [loading, setLoading] = useState(false)
-    const [buttonDisabled, setButtonDisabled] = useState(true)
+    const Router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const [buttonDisabled, setButtonDisabled] = useState(true);
     const [user, setUser] = useState({
         fullname: "",
         email: "",
         password: "",
         confirm_password: "",
-    })
+    });
 
     const handleRegistration = (e) => {
-        e.preventDefault()
-        setButtonDisabled(false)
-        toast.success('button succesfully')
+        e.preventDefault();
+        setButtonDisabled(false);
+        toast.success("button succesfully");
         // alert('button click huwa')
-    }
+    };
     useEffect(() => {
         if (
-            user.email.length > 0 && user.password.length > 0 && user.confirm_password.length > 0 && user.fullname.length > 0
+            user.email.length > 0 &&
+            user.password.length > 0 &&
+            user.confirm_password.length > 0 &&
+            user.fullname.length > 0
         ) {
-            setButtonDisabled(false)
+            setButtonDisabled(false);
         } else {
-            setButtonDisabled(true)
+            setButtonDisabled(true);
         }
-    }, [user])
+    }, [user]);
     return (
         <>
             <div>
@@ -113,13 +115,13 @@ const SingupForm = () => {
             </div>
 
             {/* <!-- component --> */}
-            <div class="bg-grey-lighter min-h-screen flex flex-col">
-                <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                    <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                        <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+            <div className="bg-grey-lighter min-h-screen flex flex-col">
+                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                    <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                        <h1 className="mb-8 text-3xl text-center">Sign up</h1>
                         <input
                             type="text"
-                            class="block border border-grey-light w-full p-3 rounded mb-4"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
                             name="fullname"
                             placeholder="Full Name"
                             value={user.fullname}
@@ -127,64 +129,78 @@ const SingupForm = () => {
                         />
 
                         <input
-                            type="text"
-                            class="block border border-grey-light w-full p-3 rounded mb-4"
+                            type="email"
                             name="email"
-                            placeholder="Email" value={user.email}
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
+                            placeholder="Email"
+                            value={user.email}
                             onChange={(e) => setUser({ ...user, email: e.target.value })}
                         />
 
-
                         <input
                             type="password"
-                            class="block border border-grey-light w-full p-3 rounded mb-4"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
                             name="password"
                             placeholder="Password"
                             value={user.password}
                             onChange={(e) => setUser({ ...user, password: e.target.value })}
-
                         />
                         <input
                             type="password"
-                            class="block border border-grey-light w-full p-3 rounded mb-4"
+                            className="block border border-grey-light w-full p-3 rounded mb-4"
                             name="confirm_password"
                             placeholder="Confirm Password"
                             value={user.confirm_password}
-                            onChange={(e) => setUser({ ...user, confirm_password: e.target.value })}
+                            onChange={(e) =>
+                                setUser({ ...user, confirm_password: e.target.value })
+                            }
                         />
 
-                        {
-                            buttonDisabled ? (
-                                <button
-                                    onClick={(e) => e.preventDefault()}
-                                    type="submit"
-                                    class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-                                >fill All Feild</button>
-                            ) : (
-                                <button
-                                    onClick={handleRegistration}
-                                    type="submit"
-                                    class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-                                >Create Account</button>
-                            )
-                        }
+                        {buttonDisabled ? (
+                            <button
+                                onClick={(e) => e.preventDefault()}
+                                type="submit"
+                                className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                            >
+                                fill All Feild
+                            </button>
+                        ) : (
+                            <button
+                                onClick={handleRegistration}
+                                type="submit"
+                                className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                            >
+                                Create Account
+                            </button>
+                        )}
 
-                        <div class="text-center text-sm text-grey-dark mt-4">
+                        <div className="text-center text-sm text-grey-dark mt-4">
                             By signing up, you agree to the
-                            <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                            <a
+                                className="no-underline border-b border-grey-dark text-grey-dark"
+                                href="#"
+                            >
                                 Terms of Service
-                            </a> and
-                            <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                            </a>{" "}
+                            and
+                            <a
+                                className="no-underline border-b border-grey-dark text-grey-dark"
+                                href="#"
+                            >
                                 Privacy Policy
                             </a>
                         </div>
                     </div>
 
-                    <div class="text-grey-dark mt-6">
+                    <div className="text-grey-dark mt-6">
                         Already have an account?
-                        <a class="no-underline border-b border-blue text-blue" href="../login/">
+                        <a
+                            className="no-underline border-b border-blue text-blue"
+                            href="../login/"
+                        >
                             Log in
-                        </a>.
+                        </a>
+                        .
                     </div>
                 </div>
             </div>
@@ -228,7 +244,7 @@ const SingupForm = () => {
                 </div>
             </section> */}
         </>
-    )
-}
+    );
+};
 
-export default SingupForm
+export default SingupForm;
