3c73558a5f03a39969745eb54aed978e34c55e7f
Muhammad Usman <usman.latif762@gmail.com>
2023-09-14 21:03:47 +0500

    updated

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginForm = () => {
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    })
     return (
-        <div>LoginForm</div>
+        <div>
+            <input
+                className="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
+                label="Email"
+                placeholder="ex: john@anymail.com"
+                inputType="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+            />
+            <input
+                className="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
+                label="Password"
+                placeholder="- - - - - -"
+                inputType="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+            />
+        </div>
     )
 }
 
