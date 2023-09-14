ec67cb18846f8d5a07be3b88d27dbfee9f9e9e70
Muhammad Usman <usman.latif762@gmail.com>
2023-09-14 21:08:01 +0500

    updated

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -7,22 +7,30 @@ const LoginForm = () => {
     })
     return (
         <>
-            <input
-                className="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
-                label="Email"
-                placeholder="ex: john@anymail.com"
-                inputType="email"
-                value={user.email}
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-            />
-            <input
-                className="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
-                label="Password"
-                placeholder="- - - - - -"
-                inputType="password"
-                value={user.password}
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-            />
+            <div className='flex flex-col gap-y-4 w-[450px] sm:w-[500px] mx-auto  px-10 py-16 rounded-md flex flex-col gap-y-6 border-[1px] border-primaryColor/30'>
+
+                <div>
+                    <p className="text-3xl font-semibold ">Login Form</p>
+
+                </div>
+
+                <input
+                    className="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
+                    label="Email"
+                    placeholder="ex: john@anymail.com"
+                    inputType="email"
+                    value={user.email}
+                    onChange={(e) => setUser({ ...user, email: e.target.value })}
+                />
+                <input
+                    className="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
+                    label="Password"
+                    placeholder="- - - - - -"
+                    inputType="password"
+                    value={user.password}
+                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                />
+            </div>
         </>
     )
 }
